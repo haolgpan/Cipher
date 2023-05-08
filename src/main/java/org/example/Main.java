d5270212fd99e1a0765d0568322d33011d8b67d9
Hao <haolgpan@gmail.com>
2023-05-08 18:36:10 +0200

    Done 2!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,18 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.util.Base64;
+import java.util.Enumeration;
+import java.util.Scanner;
+
+import static org.example.UtilitatsXifrar.getPublicKey;
 
 public class Main {
     public static void main(String[] args) {
         UtilitatsXifrar ux = new UtilitatsXifrar();
+        Scanner sc = new Scanner(System.in);
         //ACT5----------------------------------------------------------------------------------------------------------
         String text = "En Jonathan té una melena que no veas.";
         int keySize = 128;
@@ -113,6 +121,185 @@ public class Main {
         }
         if (sK == null) {
             System.out.println("No s'ha pogut trobar la clau correcta");
+        }
+
+        //A5 Act 1.1----------------------------------------------------------------------------------------------------
+        //Genera un parell de claus (KeyPair) de 1024bits, i utilitza-les per xifrar i desxifrar
+        //un missatge.
+        try {
+            // Generar parell de claus de 1024 bits
+            KeyPair keys = ux.randomGenerate(1024);
+            PublicKey pubKey = keys.getPublic();
+            PrivateKey privKey = keys.getPrivate();
+            //Act 1.3
+            System.out.println("Solució per act 1.iii");
+            System.out.println(pubKey.getAlgorithm() + "\n" +
+            privKey.getAlgorithm() + "\n" +
+            pubKey.getFormat() + "\n" +
+            privKey.getFormat() + "\n" +
+            pubKey.getClass() + "\n" +
+            privKey.getClass() + "\n" +
+                    pubKey + "\n" +
+                    privKey);
+
+
+            // Missatge a xifrar
+            System.out.println("Solució per act 1.i i 1.ii");
+            System.out.println("Introdueix text a xifrar:");
+            //Act 1.2
+            text = sc.nextLine();
+
+            // Xifrar el missatge amb la clau pública
+            encryptedData = ux.encryptDataPublic(pubKey, text.getBytes());
+
+            // Desxifrar el missatge amb la clau privada
+            decryptedData = ux.decryptDataPrivate(privKey, encryptedData);
+            decryptedText = new String(decryptedData);
+
+
+            // Mostrar resultats
+            System.out.println("Missatge original: " + text);
+            System.out.println("Missatge xifrat: " + new String(encryptedData));
+            System.out.println("Missatge desxifrat: " + decryptedText);
+        } catch (Exception ex) {
+            System.err.println("Error: " + ex);
+        }
+
+        //A5 Act 2 -----------------------------------------------------------------------------------------------------
+        String alias = "";
+        String keystorePwd = "cocopower";
+        KeyStore ks = null;
+        try {
+
+            ks = ux.loadKeyStore("keystore_nickname.ks", keystorePwd);
+
+            // 1. Tipus de keystore
+            System.out.println("Keystore Type: " + ks.getType());
+
+            // 2. Mida del magatzem
+            System.out.println("Keystore Size: " + ks.size());
+
+            // 3. Àlies de totes les claus emmagatzemades
+            Enumeration<String> aliases = ks.aliases();
+            System.out.println("Keystore Aliases: ");
+            while (aliases.hasMoreElements()) {
+                alias = aliases.nextElement();
+                System.out.println(alias);
+            }
+
+            // 4. El certificat d’una de les claus
+            Certificate cert = ks.getCertificate("mykey2");
+            System.out.println("Certificate for alias " + "mykey2" + ": ");
+            System.out.println(cert.toString());
+
+            // 5. L'algorisme de xifrat d’alguna de les claus
+            Key key = ks.getKey("mykey2", keystorePwd.toCharArray());
+            String algorithm = key.getAlgorithm();
+            System.out.println("Key Algorithm for alias " + "mykey2" + ": " + algorithm);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Genera una nova clau simètrica
+        keySize = 128;
+        SecretKey sKeyA5 = ux.keygenKeyGeneration(keySize);
+
+        // Store the key in the keystore
+       String newAlias = "haoKey";
+        try {
+            ks = ux.loadKeyStore("keystore_nickname.ks", keystorePwd);
+            KeyStore.SecretKeyEntry entry = new KeyStore.SecretKeyEntry(sKeyA5);
+            KeyStore.ProtectionParameter protParam = new KeyStore.PasswordProtection(keystorePwd.toCharArray());
+            ks.setEntry(newAlias, entry, protParam);
+            // Save the keystore to a file
+            FileOutputStream out = new FileOutputStream("keystore_nickname.ks");
+            ks.store(out, keystorePwd.toCharArray());
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //A5 Act 3 -----------------------------------------------------------------------------------------------------
+        try {
+            PublicKey publicKey = getPublicKey("mycert.cer");
+            System.out.println("Dades del certificat mycert.cer:");
+            System.out.println(publicKey.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //A5 Act 4 -----------------------------------------------------------------------------------------------------
+        PublicKey publicKey = null;
+        try {
+            ks = ux.loadKeyStore("keystore_nickname.ks", "cocopower");
+            Key key = ks.getKey("mykey2", keystorePwd.toCharArray());
+            System.out.println(key.getClass().getName());
+            publicKey = getPublicKey(ks, "mykey2", "cocopower");
+            System.out.println(publicKey.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //A5 Act 5 -----------------------------------------------------------------------------------------------------
+        byte[] data = null;
+        byte[] signature = null;
+        Path path = Paths.get("dades.txt");
+        try {
+            // llegir la PrivateKey del keystore
+            ks = ux.loadKeyStore("keystore_nickname.ks", "cocopower");
+            PrivateKey privateKey = ux.getPrivateKey(ks, "mykey2", "cocopower");
+
+            // llegir les dades a signar
+            data = Files.readAllBytes(path);
+
+            // signar les dades amb la PrivateKey
+            signature = ux.signData(data, privateKey);
+
+            // imprimir la signatura
+            System.out.println("Signatura: " + new String(signature));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //A5 Act 6 -----------------------------------------------------------------------------------------------------
+        boolean isValid = ux.validateSignature(data, signature, publicKey);
+        if (isValid) {
+            System.out.println("La signatura és vàlida.");
+        } else {
+            System.out.println("La signatura no és vàlida.");
+        }
+
+        try {
+            ks = ux.loadKeyStore("keystore_nickname.ks", "cocopower");
+            publicKey = getPublicKey(ks, "lamevaclaum9", "cocopower");
+            isValid = ux.validateSignature(data, signature, publicKey);
+            if (isValid) {
+                System.out.println("La signatura és vàlida.");
+            } else {
+                System.out.println("La signatura no és vàlida.");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        PrivateKey privateKey = null;
+        byte[][] dadesEmbolcallades = null;
+        byte[] dadesDesembolcat = null;
+        try {
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+            kpg.initialize(2048);
+            KeyPair keyPair = kpg.generateKeyPair();
+            publicKey = keyPair.getPublic();
+            privateKey = keyPair.getPrivate();
+            data = Files.readAllBytes(path);
+            dadesEmbolcallades = ux.encryptWrappedData(data,publicKey);
+            System.out.println("Text xifrat embolcallat: " + new String(dadesEmbolcallades[0]));
+            dadesDesembolcat = ux.decryptWrappedData(dadesEmbolcallades,privateKey);
+            System.out.println("Text desxifrat embolcallat: " + new String(dadesDesembolcat));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
     }
--- src/main/java/org/example/UtilitatsXifrar.java
+++ src/main/java/org/example/UtilitatsXifrar.java
@@ -2,9 +2,13 @@ package org.example;
 
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.InvalidKeyException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.*;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPrivateCrtKey;
+import java.security.spec.RSAPublicKeySpec;
 import java.util.Arrays;
 
 public class UtilitatsXifrar {
@@ -12,7 +16,7 @@ public class UtilitatsXifrar {
     //ACT1--------------------------------------------------------------------------------------------------------------
     public SecretKey keygenKeyGeneration(int keySize) {
         SecretKey sKey = null;
-        if ((keySize == 128)||(keySize == 192)||(keySize == 256)) {
+        if ((keySize == 128) || (keySize == 192) || (keySize == 256)) {
             try {
                 KeyGenerator kgen = KeyGenerator.getInstance("AES");
                 kgen.init(keySize);
@@ -28,12 +32,12 @@ public class UtilitatsXifrar {
     //ACT2--------------------------------------------------------------------------------------------------------------
     public SecretKey passwordKeyGeneration(String text, int keySize) {
         SecretKey sKey = null;
-        if ((keySize == 128)||(keySize == 192)||(keySize == 256)) {
+        if ((keySize == 128) || (keySize == 192) || (keySize == 256)) {
             try {
                 byte[] data = text.getBytes("UTF-8");
                 MessageDigest md = MessageDigest.getInstance("SHA-256");
                 byte[] hash = md.digest(data);
-                byte[] key = Arrays.copyOf(hash, keySize/8);
+                byte[] key = Arrays.copyOf(hash, keySize / 8);
                 sKey = new SecretKeySpec(key, "AES");
             } catch (Exception ex) {
                 System.err.println("Error generant la clau:" + ex);
@@ -48,8 +52,8 @@ public class UtilitatsXifrar {
         try {
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, sKey);
-            encryptedData =  cipher.doFinal(data);
-        } catch (Exception  ex) {
+            encryptedData = cipher.doFinal(data);
+        } catch (Exception ex) {
             System.err.println("Error xifrant les dades: " + ex);
         }
         return encryptedData;
@@ -68,6 +72,173 @@ public class UtilitatsXifrar {
             throw new RuntimeException(e);
         } catch (InvalidKeyException e) {
             throw new RuntimeException(e);
+        }
+        return decryptedData;
+    }
+
+    //A5 Act1-----------------------------------------------------------------------------------------------------------
+    //Afegeix a la classe d’utilitats de criptografia de l’activitat A4 el mètode 1.2.1
+    //randomGenerate del apunts.
+    public KeyPair randomGenerate(int len) {
+        KeyPair keys = null;
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(len);
+            keys = keyGen.genKeyPair();
+        } catch (Exception ex) {
+            System.err.println("Generador no disponible.");
+        }
+        return keys;
+    }
+
+    public byte[] encryptDataPublic(PublicKey sKey, byte[] data) {
+        byte[] encryptedData = null;
+        try {
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE, sKey);
+            encryptedData = cipher.doFinal(data);
+        } catch (Exception ex) {
+            System.err.println("Error xifrant les dades: " + ex);
+            ex.printStackTrace();
+        }
+        return encryptedData;
+    }
+
+    public byte[] decryptDataPrivate(PrivateKey sKey, byte[] encryptedData) {
+        byte[] decryptedData = null;
+        try {
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.DECRYPT_MODE, sKey);
+            decryptedData = cipher.doFinal(encryptedData);
+        } catch (BadPaddingException | NoSuchAlgorithmException | NoSuchPaddingException ex) {
+            System.out.println("Error desxifrant les dades: " + ex);
+        } catch (IllegalBlockSizeException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return decryptedData;
+    }
+
+    //A5 Act 2----------------------------------------------------------------------------------------------------------
+    public KeyStore loadKeyStore(String ksFile, String ksPwd) throws Exception {
+        KeyStore ks = KeyStore.getInstance("JKS");
+        File f = new File(ksFile);
+        if (f.isFile()) {
+            FileInputStream in = new FileInputStream(f);
+            ks.load(in, ksPwd.toCharArray());
+        }
+        return ks;
+    }
+
+    //A5 Act 3 ---------------------------------------------------------------------------------------------------------
+    public static PublicKey getPublicKey(String fitxer) throws Exception {
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        FileInputStream fis = new FileInputStream(fitxer);
+        X509Certificate cert = (X509Certificate) cf.generateCertificate(fis);
+        return cert.getPublicKey();
+    }
+
+    public static PublicKey getPublicKey(KeyStore ks, String alias, String pwMyKey) throws Exception {
+        Key key = ks.getKey(alias, pwMyKey.toCharArray());
+        if (key == null) {
+            throw new Exception("No s'ha trobat la clau amb àlies " + alias);
+        }
+        if (!(key instanceof RSAPrivateCrtKey)) {
+            throw new Exception("La clau amb àlies " + alias + " no és una clau RSA privada");
+        }
+        RSAPrivateCrtKey privateKey = (RSAPrivateCrtKey) key;
+        RSAPublicKeySpec spec = new RSAPublicKeySpec(privateKey.getModulus(), privateKey.getPublicExponent());
+        KeyFactory factory = KeyFactory.getInstance("RSA");
+        return factory.generatePublic(spec);
+    }
+
+    public static PrivateKey getPrivateKey(KeyStore ks, String alias, String pwMyKey) {
+        Key key = null;
+        try {
+            key = ks.getKey(alias, pwMyKey.toCharArray());
+            if (key == null) {
+                throw new Exception("No s'ha trobat la clau amb àlies " + alias);
+            }
+            if (!(key instanceof PrivateKey)) {
+                throw new Exception("La clau amb àlies " + alias + " no és una clau privada");
+            }
+
+
+        } catch (UnrecoverableKeyException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return (PrivateKey) key;
+    }
+
+    //Generació de firma digital RSA
+    public byte[] signData(byte[] data, PrivateKey priv) {
+        byte[] signature = null;
+
+        try {
+            Signature signer = Signature.getInstance("SHA1withRSA");
+            signer.initSign(priv);
+            signer.update(data);
+            signature = signer.sign();
+        } catch (Exception ex) {
+            System.err.println("Error signant les dades: " + ex);
+        }
+        return signature;
+    }
+
+    //Validació de firma digital RSA
+    public boolean validateSignature(byte[] data, byte[] signature, PublicKey pub) {
+        boolean isValid = false;
+        try {
+            Signature signer = Signature.getInstance("SHA1withRSA");
+            signer.initVerify(pub);
+            signer.update(data);
+            isValid = signer.verify(signature);
+        } catch (Exception ex) {
+            System.err.println("Error validant les dades: " + ex);
+        }
+        return isValid;
+    }
+
+    //A5 Act Clau embolcallada------------------------------------------------------------------------------------------
+    public byte[][] encryptWrappedData(byte[] data, PublicKey pub) { // Dades i clau pública del receptor
+        byte[][] encWrappedData = new byte[2][];
+        try {
+            KeyGenerator kgen = KeyGenerator.getInstance("AES");
+            kgen.init(128);
+            SecretKey sKey = kgen.generateKey(); // Genera clau simètrica
+            Cipher cipher = Cipher.getInstance("AES"); // Algorisme de xifrat simètric amb AES
+            cipher.init(Cipher.ENCRYPT_MODE, sKey); // Configura amb la clau simètrica sKey
+            byte[] encMsg = cipher.doFinal(data); // Encriptació de les dades simmètricament
+            cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding"); // Algorisme de xifrat asimètric
+            cipher.init(Cipher.WRAP_MODE, pub); // Configura amb la clau pública del receptor
+            byte[] encKey = cipher.wrap(sKey); // Clau simètrica xifrada
+            encWrappedData[0] = encMsg;
+            encWrappedData[1] = encKey;
+        } catch (Exception ex) {
+            System.err.println("Ha succeït un error xifrant: " + ex);
+        }
+        return encWrappedData; //Dades embolcallades.
+    }
+
+    public byte[] decryptWrappedData(byte[][] encWrappedData, PrivateKey priv) { //Dades xifrades amb clau xifrada i clau privada del receptor
+        byte[] decryptedData = null;
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding"); // Algorisme de xifrat asimètric
+            cipher.init(Cipher.UNWRAP_MODE, priv); // Clau privada del receptor
+            Key key = cipher.unwrap(encWrappedData[1], "AES", Cipher.SECRET_KEY); // Algorisme de xifrat simètric on extreu la clau simètrica amb la clau privada del receptor
+            cipher = Cipher.getInstance("AES"); // Configurar per desencriptar amb algorisme simètric AES
+            cipher.init(Cipher.DECRYPT_MODE, key); // Configurar en mode de desencriptació amb la clau simètrica
+            decryptedData = cipher.doFinal(encWrappedData[0]); // Dades desencriptades
+        } catch (Exception ex) {
+            System.err.println("Ha succeït un error desxifrant: " + ex);
         }
         return decryptedData;
     }
